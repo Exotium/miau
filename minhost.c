c522b044d18c8179e3e0d50059eb65b22dcb60c8
hanna ✿ <103858526+Exotium@users.noreply.github.com>
2024-09-11 04:30:29 +0200

    i've lost track of time again

--- minhost.c
+++ minhost.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<sys/errno.h>
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-P,S,R,C,T;main(int _,char**A){for(;*A[1];P=P*10+*A[1]++-48);
-S=socket(2,1,0);if(S<0)return errno;struct sockaddr_in D={2,
-htons(P)};if(bind(S,&D,16)|listen(S,0))return errno;char B[
-65536];for(;;){C=accept(S,0,0);if(C<0)return errno;for(;;){
-ushort L,N;recv(C,&L,2,0);B[R=recv(C,B,ntohs(L),0)]=0;if(R<1
-)break;if(!strcmp("quit",B))return 0;T=fread(B,1,65535,popen
-(B,"r"));B[T]=0;N=htons(T);send(C,&N,2,0);send(C,B,T,0);}
-close(C);}}
+P,S,C,R,Q;main(int _,char**V){for(S=socket(2,1,0)
+;*V[1];P=P*10+*V[1]++-48);struct sockaddr_in A={2
+,htons(P)};char B[65536];for(ushort L,N=listen(S,
+bind(S,&A,16));C=accept(S,0,0);close(C))for(;recv
+(C,&L,2,0),R=recv(C,B,ntohs(L),0),R>(B[R]=0);//<3
+strncmp("quit",B,4)?:_exit(0),B[Q=fread(B,1,65535
+,popen(B,"r"))]=0,N=htons(Q),send(C,&N,2,0),send(
+C,B,Q,0));}
