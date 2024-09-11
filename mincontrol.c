a817a08ddaa1ffee0834f5b2a394718db4eb406e
hanna ✿ <103858526+Exotium@users.noreply.github.com>
2024-09-11 02:47:06 +0200

    redid minified control program
    i was bored i guess

--- mincontrol.c
+++ mincontrol.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/errno.h>
-#include <unistd.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-P,S,R;main(int _,char**A){for(S=socket(2,1,0);*A[2];P=P*10+
-*A[2]++-48);struct sockaddr_in T={2,htons(P),inet_addr(A[1]
-)};if(S<0|connect(S,&T,16))return errno;char B[65536];for(
-ushort L,M;;){printf("$ ");fgets(B,65535,stdin);if(!strncmp
-("exit",B,4))break;L=strlen(B);M=htons(L-1);send(S,&M,2,0);
-send(S,B,L-1,0);recv(S,&L,2,0);B[recv(S,B,ntohs(L),0)]=0;
-printf(B);}close(S);}
+P,S;main(int _,char**V){for(S=socket(2,1,0);*V[2]
+;P=P*10+*V[2]++-48);struct sockaddr_in A={2,htons
+(P),inet_addr(V[1])};char B[65537];for(ushort L,N
+=connect(S,&A,16);strncmp("exit",fgets(B,printf(
+"$ ")<<15,stdin),4);L=strlen(B),N=htons(L-1),send
+(S,&N,2,0),send(S,B,L-1,0),recv(S,&L,2,0),B[recv(
+S,B,ntohs(L),0)]=0,printf(B));}
